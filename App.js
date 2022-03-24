6dfa796f6011c36a5f8ede49a4c7aaa3a061740b
Mazen Khenaisser <mazenonios@Mazens-MacBook-Pro.local>
2022-03-24 09:45:43 -0700

    made app a class

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { Text, View } from 'react-native';
 
-export default function App() {
+class App extends Component {
+  render() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>No Problem Here</Text>
     </View>
   );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+
